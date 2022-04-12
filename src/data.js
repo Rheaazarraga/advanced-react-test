e1378aadde72eb0c45a4327cf117defdd4dd2aea
Rhea <rheaazarraga@gmail.com>
2022-04-11 18:54:11 -0700

    change import to start useState array lesson, change names in data array

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const data = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
+  { id: 1, name: 'bita' },
+  { id: 2, name: 'rhea' },
+  { id: 3, name: 'alex' },
+  { id: 4, name: 'musk' },
 ];
