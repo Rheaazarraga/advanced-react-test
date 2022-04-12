22f655242054478e2156f72e9e4663ee7fced217
Rhea <rheaazarraga@gmail.com>
2022-04-11 19:44:33 -0700

    render useState object and implement logic to manipulate a single property in the oject using spread operator

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [ person, setPerson ] = useState({ name: 'bita', age: 7, msg: 'hey bita!' });
+  console.log(person);
+
+  const changeMsg = () => {
+    /* use spread operator to copy the person object, but only target the msg 
+    property to change the msg on the screen and prevent the rest of the object data
+     from being wiped */
+    setPerson({ ...person, msg:'are you a baby?' });
+  }
+  return (
+  <>
+  <h3>{person.name}</h3>
+  <h3>{person.age}</h3>
+  <h3>{person.msg}</h3>
+  <button className='btn' onClick={changeMsg}>change msg</button>
+  </>
+  )
 };
 
 export default UseStateObject;
