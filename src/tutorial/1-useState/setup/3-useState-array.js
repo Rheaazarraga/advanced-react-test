6856c08db0443a28af3b623c73b3efdb4a7a7367
Rhea <rheaazarraga@gmail.com>
2022-04-11 19:30:46 -0700

    add reference function to remove individual items/ names in the list

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,18 +2,29 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  const [people, setPeople] = React.useState(data);
+  const [ people, setPeople ] = React.useState(data);
+
+  const removeItem = (id) => {
+    // filter out the default value array of names
+    // if the person id matches the one being passed in, remove it
+    // return only the list of names that don't match the passed in person id
+    let newPeople = people.filter((person) => person.id !== id)
+    // update 
+    setPeople(newPeople);
+  };
+
   return (
     <>
-    {people.map((person) => {
-      const { id, name } = person;
-      return (
-        <div key={id} className='item'>
-          <h4>{name}</h4>
-        </div>
-      );
-    })}
-    <button className='btn' onClick={() => setPeople([])}>clear items</button>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button className='btn' onClick={() => setPeople([])}>clear items</button>
     </>
   )
 };
