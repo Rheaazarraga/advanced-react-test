80255581093299d36a85cde713493952229f1f6a
Rhea <rheaazarraga@gmail.com>
2022-04-11 19:53:45 -0700

    create multiple state values as an alternative to using objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -8,13 +8,23 @@ const UseStateObject = () => {
     /* use spread operator to copy the person object, but only target the msg 
     property to change the msg on the screen and prevent the rest of the object data
      from being wiped */
-    setPerson({ ...person, msg:'are you a baby?' });
-  }
+
+    // setPerson({ ...person, msg:'are you a baby?' });
+
+    setMsg('are you a baby?');
+  };
+
+  // alternative to using objects - especially if they're big, set up multiple smaller state values with a single value and a function that controls it
+  const [name, setName] = useState('bita');
+  const [age, setAge] = useState(7);
+  const [msg, setMsg] = useState('hey!');
+
+
   return (
   <>
-  <h3>{person.name}</h3>
-  <h3>{person.age}</h3>
-  <h3>{person.msg}</h3>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h3>{msg}</h3>
   <button className='btn' onClick={changeMsg}>change msg</button>
   </>
   )
