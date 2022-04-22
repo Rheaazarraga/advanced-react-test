789ad577e5c8b4ae6d6382f3036efa7a3d2d51e6
Rhea <rheaazarraga@gmail.com>
2022-04-22 16:34:34 -0700

    add inline onclick function for incrementing and decrementing counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -8,8 +8,8 @@ const UseStateCounter = () => {
     <section style={{margin:'4rem 0'}}>
       <h2>regular counter</h2>
       <h1>{value}</h1>
-      <button className='btn'>increase</button>
-      <button className='btn'>decrease</button>
+      <button className='btn' onClick={()=> setValue(value+1)}>increase</button>
+      <button className='btn'onClick={()=> setValue(value-1)}>decrease</button>
       <button className='btn'>reset</button>
     </section>
     </>
