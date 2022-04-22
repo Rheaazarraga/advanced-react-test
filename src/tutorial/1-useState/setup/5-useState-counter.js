757d4d5dc6eb9b2c0ce437eb3735e19f56eea63e
Rhea <rheaazarraga@gmail.com>
2022-04-22 16:36:25 -0700

    create reference reset function and pass the value into the button component

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
 
+  const reset = () => {
+    setValue(0);
+  };
+
   return (
     <>
     <section style={{margin:'4rem 0'}}>
@@ -10,7 +14,7 @@ const UseStateCounter = () => {
       <h1>{value}</h1>
       <button className='btn' onClick={()=> setValue(value+1)}>increase</button>
       <button className='btn'onClick={()=> setValue(value-1)}>decrease</button>
-      <button className='btn'>reset</button>
+      <button className='btn'onClick={reset}>reset</button>
     </section>
     </>
   );
