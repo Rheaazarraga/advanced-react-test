296aad0219fe535704e69b06741509065cafc5f0
Rhea <rheaazarraga@gmail.com>
2022-04-24 15:14:37 -0700

    add second parameter dependency list of value

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,20 +3,20 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  const [value, setValue] = useState(0);
+  const [ value, setValue ] = useState(0);
 
   // *** hooks cannot be placed in conditionals, must be within useEffect ***
   useEffect(() => {
     console.log('call useEffect');
-    if(value >= 1) {
-    document.title = `New Msgs(${value})`;
+    if (value >= 1) {
+      document.title = `New Msgs(${value})`;
     }
-  })
+  }, [value])
   console.log('render component"');
   return (
     <>
-    <h1>{value}</h1>
-    <button className='btn' onClick={()=> setValue(value +1)}>Click me!</button>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>Click me!</button>
     </>
   )
 };
