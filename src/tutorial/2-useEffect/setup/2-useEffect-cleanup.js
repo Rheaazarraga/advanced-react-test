5e46f9939f4c8da01fb11e4decb120e5eb0f31ed
Rhea <rheaazarraga@gmail.com>
2022-04-24 15:21:40 -0700

    add checkSize function, event listener and comments

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -5,9 +5,24 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectCleanup = () => {
   // the goal will be to change the value as the browser window changes
-  const [size, setSize] = useState(window.innerWidth);
-  console.log(size);
-  return <h2>useEffect cleanup</h2>;
+  const [ size, setSize ] = useState(window.innerWidth);
+  // console.log(size);
+
+  const checkSize = () => {
+    // should update the size value
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    // every time resize event takes place, the checkSize event function is invoked
+    window.addEventListener('resize', checkSize)
+  })
+
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2></>
+  );
 };
 
 export default UseEffectCleanup;
