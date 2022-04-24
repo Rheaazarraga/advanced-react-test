58ca7f42d1259b952fd69164819ffe2693249393
Rhea <rheaazarraga@gmail.com>
2022-04-24 15:17:57 -0700

    add useEffect

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,6 +4,9 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
+  // the goal will be to change the value as the browser window changes
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
   return <h2>useEffect cleanup</h2>;
 };
 
