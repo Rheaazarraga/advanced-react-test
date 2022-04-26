886747655ecac23244a5059a600c06092ac4d8c7
Rhea <rheaazarraga@gmail.com>
2022-04-26 13:50:35 -0700

    create users function and start of useEffect hook

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,24 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [ users, setUsers ] = useState([]);
+
+  const getUsers = async() => {
+    const response = await fetch(url);
+    const users = await response.json();
+    // setUsers(users); will update the default users value - will cause an infinite loop because the useEffect will trigger a re-render and 
+    console.log(users);
+  }
+
+  useEffect(() => {
+    // perform fetch request
+    // cannot make useEffect function async await because it returns a promise
+    getUsers();
+  })
+  return (
+    <>
+      <h3>github users</h3>
+    </>);
 };
 
 export default UseEffectFetchData;
