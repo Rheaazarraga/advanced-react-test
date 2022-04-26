cf0e69dd9d1b33b6d5976d924490005f9ed2d20d
Rhea <rheaazarraga@gmail.com>
2022-04-26 14:34:27 -0700

    add additional useStates and conditionals

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  const [ loading, setLoading ] = useState(false);
+  const [ isloading, setIsLoading ] = useState(true);
+  const [ isError, setIsError ] = useState(false);
+  const [ user, Setuser ] = useState('default user');
 
-  if (loading) {
-    return <h2>Loading...</h2>
+
+  if (isloading) {
+    return (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+    );
+  }
+  
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
   }
 
   return <h2>multiple returns</h2>;
